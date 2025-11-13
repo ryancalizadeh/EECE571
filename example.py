a682a0bce128898cf86ba087430528fc734894a5
Ryan Alizadeh <ryancalizadeh@gmail.com>
2025-11-13 08:37:43 -0800

    initial commit

---
+++ example.py
@@ -0,0 +1,24 @@
+import matplotlib
+matplotlib.use('Qt5Agg')
+
+import andes
+import matplotlib.pyplot as plt
+#from andes.utils.paths import get_case
+
+if __name__ == '__main__':
+    andes.config_logger(stream_level=50)
+    ss = andes.run('kundur_full.xlsx', default_config=True)
+    ss.TDS.config.tf = 20
+    ss.TDS.run()
+    print(ss.exit_code)
+    if ss.TDS.load_plotter() is None:
+        ss.TDS.load_plotter()
+
+    fig, ax = ss.TDS.plt.plot((5, 6, 7, 8))
+    fig.savefig("plot.png", dpi=300)
+
+    fig, ax = ss.TDS.plt.plot(ss.GENROU.omega, a=(0, 3))
+    fig.savefig("plot_omega.png", dpi=300)
+
+    fig, ax = ss.TDS.plt.plot((9, 10, 11, 12))
+    fig.savefig("e_plot.png", dpi=300)
